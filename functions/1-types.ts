b4fde2d2efcb5acf5637e181a0283d7248f98307
Xavier Kirkpatrick <kirkpatrickxavier@gmail.com>
2024-07-03 18:35:22 +1200

    complete sumMatchingType function + pass all test

--- functions/1-types.ts
+++ functions/1-types.ts
@@ -77,16 +77,48 @@ export function addNumbersAndStrings(a: string | number, b: string | number) {
 // if the number is divisible by 5, return 'buzz'
 // if the number is divisible by 3 and 5, return 'fizzbuzz'
 // otherwise, return the original number
-export function fizzbuzz(a) {}
+export function fizzbuzz(a: number): string | number {
+
+  if (a % 3 === 0 && a % 5 === 0) {
+    return "fizzbuzz"
+  }
+  else if (a % 3 === 0) {
+    return "fizz"
+  }
+  else if (a % 5 === 0) {
+    return "buzz"
+  }
+  else {
+    return a
+  }
+}
 
 // ----------- TYPE ALIASES -----------
 
-type StringOrNumber = void
+type StringOrNumber = string | number
 
 // sumMatchingType accepts two arguments that can be either a number or a string
 // if both arguments are numbers, the sum of the two arguments should be returned as a number
 // if either argument is a string, the two arguments should be summed together and returned as a string
-export function sumMatchingType(): void {}
+export function sumMatchingType(a: StringOrNumber, b: StringOrNumber){
+  
+  if ((typeof a === 'number' && typeof b === 'string') || (typeof a === 'string' && typeof b === 'number')) {
+    const aToNum = typeof a === 'string' ? parseInt(a) : a 
+    const bToNum = typeof b === 'string' ? parseInt(b) : b
+    const addNums = aToNum + bToNum
+    return addNums.toString()
+  }
+  else if (typeof a === 'number' && typeof b === 'number') {
+    return a + b
+  }
+  else if (typeof a === 'string' && typeof b === 'string') {
+    const add = parseInt(a) + parseInt(b)
+    return add.toString()
+  }
+  else {
+    return 0
+  }
+}
 
 // getNextTrafficLightColour accepts the current colour of a traffic light colour and returns the next colour
 // the light sequence is green -> yellow -> red -> green -> yellow -> (etc)
